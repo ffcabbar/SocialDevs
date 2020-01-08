61bb52e7eab728aed73b7f1cc92dfe60ea508707
Ismail Dogan <ismaildogaan@gmail.com>
2020-01-08 14:30:06 +0300

    Infrastructure layer and Jwt Bearer Authentication tokens have been implemented.

---
+++ SocialDevs.Core/Attributes/ValidateModelAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using SocialDevs.Domain.Error;
+
+namespace SocialDevs.Core.Attributes
+{
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new ValidationFailedResult(context.ModelState);
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+
+    }
+}
---
+++ SocialDevs.Core/Security/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using SocialDevs.Domain.Api;
+using SocialDevs.Domain.Dto;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace SocialDevs.Core.Security
+{
+    public interface ITokenService
+    {
+        TokenUserResponse GenerateToken(AppUserDto appUserDto);
+
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+    }
+}
