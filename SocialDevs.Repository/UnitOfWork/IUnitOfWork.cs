e58334b255794567a075b352655c1c2a9c2312cd
Ismail Dogan <ismaildogaan@gmail.com>
2020-01-08 13:06:54 +0300

    Repository, Unit of Work and Includable design patterns have been implemented.

---
+++ SocialDevs.Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using SocialDevs.Entities.Data;
+using System;
+
+namespace SocialDevs.Repository.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int Commit();
+
+        ApplicationDbContext GetDbContext();
+    }
+}
